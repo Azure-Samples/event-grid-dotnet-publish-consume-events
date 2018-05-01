5a8a1c762d2d68cdb9a4bff799f47d4a855e9bf5
J. Kalyana Sundaram <kalyanaj@microsoft.com>
2018-04-30 17:40:42 -0700

    Added EventGridPublisher sample.

---
+++ EventGridPublisher/Program.cs
@@ -0,0 +1,74 @@
+﻿//----------------------------------------------------------------------------------
+// Microsoft Azure EventGrid Team
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+//----------------------------------------------------------------------------------
+// The example companies, organizations, products, domain names,
+// e-mail addresses, logos, people, places, and events depicted
+// herein are fictitious.  No association with any real company,
+// organization, product, domain name, email address, logo, person,
+// places, or events is intended or should be inferred.
+//----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+using Newtonsoft.Json;
+
+namespace EventGridPublisher
+{
+    class ContosoItemReceivedEventData
+    {
+        [JsonProperty(PropertyName = "itemUri")]
+        public string ItemUri { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Enter values for <topic-name> and <region> 
+            string topicEndpoint = "https://<topic-name>.<region>-1.eventgrid.azure.net/api/events";
+
+            // TODO: Enter value for <topic-key>
+            string topicKey = "<topic-key>";
+
+            string topicHostname = new Uri(topicEndpoint).Host;
+            TopicCredentials topicCredentials = new TopicCredentials(topicKey);
+            EventGridClient client = new EventGridClient(topicCredentials);
+
+            client.PublishEventsAsync(topicHostname, GetEventsList()).GetAwaiter().GetResult();
+            Console.Write("Published events to Event Grid.");
+            Console.ReadLine();
+        }
+
+        static IList<EventGridEvent> GetEventsList()
+        {
+            List<EventGridEvent> eventsList = new List<EventGridEvent>();
+
+            for (int i = 0; i < 1; i++)
+            {
+                eventsList.Add(new EventGridEvent()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    EventType = "Contoso.Items.ItemReceivedEvent",
+                    Data = new ContosoItemReceivedEventData()
+                    {
+                        ItemUri = "ContosoSuperItemUri"
+                    },
+                    EventTime = DateTime.Now,
+                    Subject = "Door1",
+                    DataVersion = "2.0"
+                });
+            }
+
+            return eventsList;
+        }
+    }
+}
+
